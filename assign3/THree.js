40385ad6e121f1c2a86c377f75a1c35f4170867c
Shrihari00 <91113813+Shrihari00@users.noreply.github.com>
2022-10-05 16:08:01 +0530

    Add files via upload

---
+++ assign3/THree.js
@@ -0,0 +1,9 @@
+document.getElementById('removeemail').style.display = 'none'
+document.querySelector('#button1').addEventListener('click',function(){
+
+    document.getElementById('removeemail').style.display ='block'
+})
+document.querySelector('#button2').addEventListener('click',function(){
+    
+    document.getElementById('removeemail').style.display ='none';
+})
